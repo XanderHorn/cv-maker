49364aa0a07e926b60d170709c50a33a7fa74d42
Xander Horn <xanderhorn@hotmail.com>
2023-08-21 21:20:17 +0200

    Add files via upload
    Remove temporary code

--- main.py
+++ main.py
@@ -8,12 +8,3 @@ data = create_template_from_excel()
 cv = render_cv(data, template)
 render_html_file(cv, 'output/cv.html')
 pdfkit.from_string(cv, 'output/cv.pdf', toc=False)
-
-
-from bs4 import BeautifulSoup
-soup = BeautifulSoup(cv, 'html.parser')
-toc_element = soup.find(str='toc')
-if toc_element:
-    toc_element.decompose()
-
-
